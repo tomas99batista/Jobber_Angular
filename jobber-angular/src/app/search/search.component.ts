6cb2e6279d023291ffa05f8e043d24ff79facb1c
Flávia <flaviafigueiredo@ua.pt>
2019-12-17 14:15:03 +0000

    presentation

--- jobber-angular/src/app/search/search.component.ts
+++ jobber-angular/src/app/search/search.component.ts
@@ -9,6 +9,18 @@ export class SearchComponent {
   title = 'Search';
   baseUrl: 'http://localhost:4200';
   searchText;
+  heroes = [
+    { title: 'Front-End Developer', job_sector: 'Tech', location: 'Aveiro', company: 'Delloite' },
+    { title: 'Back-End Developer', job_sector: 'Tech', location: 'Porto', company: 'Google' },
+    { title: 'Full-Stack Developer', job_sector: 'Tech', location: 'Lisboa', company: 'Blip' },
+    { title: 'Limpezas', job_sector: 'Outros', location: 'Bragança', company: 'Casa Limpa'},
+    { title: 'Cozinheiro', job_sector: 'Restauração', location: 'Porto', company: 'Curb'},
+    { title: 'Copeiro', job_sector: 'Restauração', location: 'Lisboa', company: '100Maneiras'},
+    { title: 'Actor', job_sector: 'Entretenimento', location: 'Lisboa', company: 'Tvi' },
+    { title: 'Jornalista', job_sector: 'Jornalismo', location: 'Lisboa', company: 'CmTV'}
+  ];
+
+
 
   constructor(private httpClient: HttpClient) {
   }
